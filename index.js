e8a6be2bc08cec05d592cdeb19226b94ab505538
Jonathan Martin <me@nybblr.com>
2018-03-29 14:33:08 -0400

    Koa version.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
@@ -7,17 +6,6 @@ const {
 } = require('./database');
 
 const signature = '1mm@_s3cur3_s3rv3r';
-
-let readBody = request =>
-  new Promise(resolve => {
-    let body = '';
-    request.on('data', chunk => {
-      body += chunk.toString();
-    });
-    request.on('end', () => {
-      resolve(body);
-    });
-  });
 
 // Create JWT for user
 let createToken = user =>
@@ -28,24 +16,26 @@ let createToken = user =>
   );
 
 // POST /tokens
-let postTokens = async (req, res) => {
-  let body = await readBody(req);
-  let creds = JSON.parse(body);
-  let { email, password } = creds;
+let postTokens = async ctx => {
+  let { email, password } = ctx.request.body;
   let user = findUserByEmail(email);
 
   let isValid = await bcrypt.compare(password, user.password);
   if (isValid) {
     let token = createToken(user);
-    res.end(token);
+    ctx.body = token;
   } else {
-    res.end('No token for you!');
+    ctx.body = 'No token for you!';
   }
 };
 
-// GET /private
-let privatePage = (req, res) => {
-  let { authorization: token } = req.headers;
+// GET /api/private
+let privatePage = ctx => {
+  ctx.body = `Muahaha welcome to the club, user #${ctx.jwt.userId}`;
+};
+
+let checkToken = async (ctx, next) => {
+  let { authorization: token } = ctx.headers;
   let payload;
   try {
     payload = jwt.verify(token, signature);
@@ -54,22 +44,29 @@ let privatePage = (req, res) => {
   }
 
   if (payload) {
-    let { userId } = payload;
-    res.end(`Muahaha welcome to the club, user #${userId}`);
+    ctx.jwt = payload;
+    await next();
   } else {
-    res.end('YOU SHALL NOT PASS');
+    ctx.body = 'YOU SHALL NOT PASS';
   }
 };
 
-let routes = {
-  'POST /tokens': postTokens,
-  'GET /private': privatePage,
-};
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
-let server = http.createServer((req, res) => {
-  let { url, method } = req;
-  let route = `${method} ${url}`;
-  routes[route](req, res);
-});
+let app = new Koa();
+let router = new Router();
 
-server.listen(3000);
+let tokensAPI = new Router();
+tokensAPI.post('/', postTokens);
+
+let api = new Router();
+api.get('/private', privatePage);
+
+router.use('/tokens', tokensAPI.routes());
+router.use('/api', checkToken, api.routes());
+
+app.use(bodyParser());
+app.use(router.routes());
+app.listen(3000);
